55a361efc0c325504015eaa84df43dad4289b808
sunhanmoo <ghdms1013@naver.com>
2024-11-05 14:28:37 +0900

    project setting

---
+++ iOS_FE/iOS_FE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_FE
+//
+//  Created by 성호은 on 11/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_FE/iOS_FETests/iOS_FETests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_FETests.swift
+//  iOS_FETests
+//
+//  Created by 성호은 on 11/5/24.
+//
+
+import Testing
+@testable import iOS_FE
+
+struct iOS_FETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
